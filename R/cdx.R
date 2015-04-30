db4b0eae9f9871730c2aa79eb9ae452035c280d2
Jeffrey Uslan <jduslan@gmail.com>
2015-04-29 22:09:56 -0700

    changed cpath

--- R/cdx.R
+++ R/cdx.R
@@ -7,39 +7,34 @@
 #'
 #'
 
-cdx<-function(targ="list") {
-
-	if(length(grep("^[Ww]in",Sys.info()[["sysname"]])))
-	{
-	  cpath="E:\\ado\\site\\"
-	} else
-	{
-		cpath="/fileserver/server/ado/site/"
+cdx<-function(targ="list",newPath=FALSE) {
+  require("dplyr")
+  cpath="C:\Program Files\R"
+  
+	if (!newPath){
+	  load(paste0(cpath,"cdxdirs.rda"))
+	  d$targ=as.character(d$targ)
+	  d$path=as.character(d$path)
+    
+	  loc=which(d$targ==targ)
+	  if(length(loc))
+	  {
+	    setwd(d$path[loc])  
+	  } else {
+	    cat("Could not find",key,"path\n")
+	  }
+	} else {
+	  if (file.exists(paste0(cpath,"cdxdirs.rda"))){
+	    load(paste0(cpath,"cdxdirs.rda"))
+	  } else{
+	    d=data.frame(targ=NULL,path=NULL) 
+	  }
+    bind_vec=data.frame(targ,as.character(choose.dir()))
+	  names(bind_vec)=c("targ","path")
+    d=rbind(d,bind_vec)
+	  d=distinct(d)
+    d=na.omit(d)
+	  names(d)=c("targ","path")
+	  save(d,file=paste0(cpath,"cdxdirs.rda"))
 	}
-
-	d=read.csv(file=paste(cpath,"cdxdirs.csv",sep=""),header=TRUE)
-	d$targ=as.character(d$targ)
-	d$path=as.character(d$path)
-
-	if(targ=="list")
-	{
-		subset(d,select=c(targ,path))
-	} else
-	{
-		loc=which(d$targ==targ)
-		if(length(loc))
-		{
-
-			if(length(grep("^[Ww]in",Sys.info()[["sysname"]])))
-			{
-			  setwd(paste0("/",d$path[loc]))
-			} else
-			{
-			  setwd(d$path[loc])
-			}
-		} else {
-			cat("Could not find",key,"path\n")
-		}
-	}
-
 }
